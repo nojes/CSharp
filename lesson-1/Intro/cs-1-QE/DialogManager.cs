2fb46169e4debf7c7e4e0e7fbd6e1cb3bdcfe26e
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-07-01 12:14:16 +0300

    class Console

---
+++ lesson-1/Intro/cs-1-QE/DialogManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_1_QE
+{
+    class DialogManager
+    {
+        int choise;
+        string ans;
+        string prefix = "\n [INFO]: ";
+
+        public int Choise
+        {
+            get { return choise; }
+        }
+
+        public void Run()
+        {
+            Console.Clear();
+            Console.WriteLine("\t Программа решения квадраиных уравнений.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("\n [INFO]: Программа завершена.");
+        }
+
+        public void DisplayMenu()
+        {
+            
+            Console.WriteLine(" <1> - Решить новое уравнение.");
+            Console.WriteLine(" <2> - Показать журнал вычислений.");
+            Console.WriteLine(" <3> - Выйти из программы.");
+            Console.Write("  -> ");
+            try
+            {
+                choise = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception err)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\n [ERROR]: {0}", err.Message);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
+
+        public bool AllowContinue()
+        {
+            Console.Write(prefix + "Продолжать? (y/n)");
+            Console.Write("  -> ");
+
+            ans = Console.ReadLine();
+            return (ans == "y");
+        }
+    }
+}
--- lesson-1/Intro/cs-1-QE/Program.cs
+++ lesson-1/Intro/cs-1-QE/Program.cs
@@ -10,6 +10,27 @@ namespace cs_1_QE
     {
         static void Main(string[] args)
         {
+            DialogManager dm = new DialogManager();
+            do
+            {
+                dm.Run();
+                dm.DisplayMenu();
+                switch (dm.Choise)
+                {
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+
+                    default:
+                        break;
+                }
+            } while (dm.AllowContinue());
+            dm.Stop();
         }
     }
 }
