feeee558ecefd4ea57b01404b9b1fc4470a811eb
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-07-06 10:18:40 +0300

    file streams

--- lesson-3/Arrays,Strings/cs-Censor/Program.cs
+++ lesson-3/Arrays,Strings/cs-Censor/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace cs_Censor
 {
@@ -10,6 +11,41 @@ namespace cs_Censor
     {
         static void Main(string[] args)
         {
+            string path = @"..\..\Data\";
+            string input_name = "input.txt";
+            string output_name = "output.txt";
+
+            string[] ban_words = new string[] {
+                "петрушка",
+                "сосиска",
+                "редиска"
+            };
+
+            string[] replace_words = new string[] {
+                "п***шка",
+                "с***ка",
+                "****cка"
+            };
+
+            FileStream input = new FileStream(path + input_name, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(input);
+            string input_text = sr.ReadToEnd();
+            sr.Close();
+            input.Close();
+
+            string output_text = input_text.Replace(ban_words[0], replace_words[0]);
+            output_text = output_text.Replace(ban_words[1], replace_words[1]);
+            output_text = output_text.Replace(ban_words[2], replace_words[2]);
+
+            FileStream output = new FileStream(path + output_name, FileMode.Open, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(output);
+            sw.Write(output_text);
+            sw.Close();
+
+            Console.WriteLine("\n Input > \n {0}", input_text);
+            Console.WriteLine("\n Output > \n {0}", output_text);
+
+            Console.ReadLine();
         }
     }
 }
