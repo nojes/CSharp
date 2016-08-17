0eb1dc52d0542daf3631e084b8d76e3eec87a7f3
nojes <vv.nojenko@gmail.com>
2016-08-17 10:53:15 +0300

    struct example

---
+++ lesson-9/Structures,Enums/cs-9-ex1/Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cs_9_ex1
+{
+    public struct Data
+    {
+        public int a;
+        public long b;
+        public double c;
+        public string d;
+        public bool e;
+
+        public Data(int a, long b, double c, string d, bool e)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.e = e;
+        }
+
+        public void Display()
+        {
+            Console.Write(String.Format(" a: {0} \n b: {1} \n c: {2} \n d: {3} \n e: {4} \n", a, b, c, d, e));
+        }
+    }
+}
--- lesson-9/Structures,Enums/cs-9-ex1/Program.cs
+++ lesson-9/Structures,Enums/cs-9-ex1/Program.cs
@@ -10,6 +10,23 @@ namespace cs_9_ex1
     {
         static void Main(string[] args)
         {
+            Data a = new Data(7, 99789797, 555.12, "data", true);
+            Data b = a;
+
+            Display(a, b);
+
+            b.a = 777;
+            b.d = "dataaaaaaa";
+
+            Display(a, b);
+        }
+
+        static void Display(Data a, Data b)
+        {
+            Console.WriteLine("\n Data a");
+            a.Display();
+            Console.WriteLine("\n Data b");
+            b.Display();
         }
     }
 }
