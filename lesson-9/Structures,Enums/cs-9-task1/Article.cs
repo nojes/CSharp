a84633a95257ed9a9f89fa2b9b95c5a409fd87af
nojes <vv.nojenko@gmail.com>
2016-08-17 11:58:44 +0300

    simple enum task

---
+++ lesson-9/Structures,Enums/cs-9-task1/Article.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cs_9_task1
+{
+    public enum ArticleType: uint
+    {
+        undefined,
+        Computers,
+        Smartphones
+    }
+
+    public struct Article
+    {
+        static uint article_id = 0;
+        public uint id;
+        public string title;
+        public double price;
+        public ArticleType type;
+
+        public Article(string title, double price, ArticleType type)
+        {
+            this.id = ++article_id;
+            this.title = title;
+            this.price = price;
+            this.type = type;
+        }
+
+        public override string ToString()
+        {
+            string articles = Enum.GetName(typeof(ArticleType), type);
+            return String.Format("\n Id: {0} \n Title: {1} \n Price: {2} \n Category: {3}", id, title, price, articles);
+        }
+    }
+}
--- lesson-9/Structures,Enums/cs-9-task1/Program.cs
+++ lesson-9/Structures,Enums/cs-9-task1/Program.cs
@@ -10,6 +10,11 @@ namespace cs_9_task1
     {
         static void Main(string[] args)
         {
+            Article a1 = new Article("Article title 1", 100, ArticleType.Computers);
+            Article a2 = new Article("Article title 2", 200, ArticleType.Smartphones);
+
+            Console.WriteLine(a1.ToString());
+            Console.WriteLine(a2.ToString());
         }
     }
 }
