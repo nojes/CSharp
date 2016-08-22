6a333bae81dbfe853ea55c1bed76ac5a47462557
nojes <vv.nojenko@gmail.com>
2016-08-22 11:54:40 +0300

    WinForm events example

--- lesson-10/Delegates,Events/cs-10-ex3/Form1.Designer.cs
+++ lesson-10/Delegates,Events/cs-10-ex3/Form1.Designer.cs
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Item1",
+            "Item2",
+            "Item3"});
+            this.comboBox1.Location = new System.Drawing.Point(55, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 144);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- lesson-10/Delegates,Events/cs-10-ex3/Form1.cs
+++ lesson-10/Delegates,Events/cs-10-ex3/Form1.cs
@@ -16,5 +16,12 @@ namespace cs_10_ex3
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+
+            int item = comboBox1.SelectedIndex;
+        }
     }
 }
