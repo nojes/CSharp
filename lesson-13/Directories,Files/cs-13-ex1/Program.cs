65685f34f1aff18865bac646345345a122d61b36
Nojes <vv.nojenko@gmail.com>
2016-08-29 09:42:56 +0300

    lesson #13: init example 1

---
+++ lesson-13/Directories,Files/cs-13-ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_13_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
