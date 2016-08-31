83d8c78a7fe7228cd4d234300ab303a115c7e209
Nojes <vv.nojenko@gmail.com>
2016-08-31 11:11:22 +0300

    Creating 'XmlElement', saving 'XmlDocument'

--- lesson-14/XML/01-XML-ex1/Program.cs
+++ lesson-14/XML/01-XML-ex1/Program.cs
@@ -13,21 +13,56 @@ namespace _01_XML_ex1
         {
             const string DATA_ROOT = "../../Data/";
 
-            // Load data
+            // Load document
             XmlDocument doc = new XmlDocument();
             doc.Load(DATA_ROOT + "data.xml");
             XmlNode root = doc.DocumentElement;
 
-            // Get data
+            // Get elements from document
             XmlNodeList nodes = doc.GetElementsByTagName("student");
 
-            // Render view
+            // Display elements
             foreach (XmlNode n in nodes) {
                 foreach (XmlAttribute a in n.Attributes) {
                     Console.WriteLine(" {0} => {1}", a.Name, a.Value);
                 }
                 Console.WriteLine();
             }
+
+            // New element
+            XmlElement s = doc.CreateElement("student");
+            XmlAttribute id = doc.CreateAttribute("id");
+            XmlAttribute fname = doc.CreateAttribute("fname");
+            XmlAttribute lname = doc.CreateAttribute("lname");
+            XmlAttribute age = doc.CreateAttribute("age");
+            XmlAttribute rate = doc.CreateAttribute("rate");
+
+            Console.WriteLine("\n Input data: ");
+
+            // Get 'new element' data 
+            Console.Write(" id: ");
+            id.Value = Console.ReadLine();
+            Console.Write(" fname: ");
+            fname.Value = Console.ReadLine();
+            Console.Write(" lname: ");
+            lname.Value = Console.ReadLine();
+            Console.Write(" age: ");
+            age.Value = Console.ReadLine();
+            Console.Write(" rate: ");
+            rate.Value = Console.ReadLine();
+
+            // Append attributes of 'new element'
+            s.Attributes.Append(id);
+            s.Attributes.Append(fname);
+            s.Attributes.Append(lname);
+            s.Attributes.Append(age);
+            s.Attributes.Append(rate);
+
+            // Append 'new element'
+            root.AppendChild(s);
+
+            // Save xml document
+            doc.Save(DATA_ROOT + "data.xml");
         }
     }
 }
