a5827464d4f0324fba715d020f9684d1ca7d1fdb
Nojes <vv.nojenko@gmail.com>
2016-08-31 10:22:54 +0300

    Document, NodeList, Node, Attribute

--- lesson-14/XML/01-XML-ex1/Program.cs
+++ lesson-14/XML/01-XML-ex1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace _01_XML_ex1
 {
@@ -10,6 +11,23 @@ namespace _01_XML_ex1
     {
         static void Main(string[] args)
         {
+            const string DATA_ROOT = "../../Data/";
+
+            // Load data
+            XmlDocument doc = new XmlDocument();
+            doc.Load(DATA_ROOT + "data.xml");
+            XmlNode root = doc.DocumentElement;
+
+            // Get data
+            XmlNodeList nodes = doc.GetElementsByTagName("student");
+
+            // Render view
+            foreach (XmlNode n in nodes) {
+                foreach (XmlAttribute a in n.Attributes) {
+                    Console.WriteLine(" {0} => {1}", a.Name, a.Value);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
