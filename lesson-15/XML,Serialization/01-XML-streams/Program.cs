a49e2f540d9e98c225bc2c455062524728ed248f
Nojes <vv.nojenko@gmail.com>
2016-09-05 10:20:11 +0300

    XmlTextReader

--- lesson-15/XML,Serialization/01-XML-streams/Program.cs
+++ lesson-15/XML,Serialization/01-XML-streams/Program.cs
@@ -11,7 +11,52 @@ namespace _01_XML_streams
     {
         static void Main(string[] args)
         {
-            
+            XmlTextReader reader = new XmlTextReader(@"../../Data/data.xml");
+
+            XmlNodeType nodeType = reader.NodeType;
+            string nodeName = reader.Name;
+            string nodeValue = reader.Value;
+            int nodeAtributeCount = reader.AttributeCount;
+
+            Console.WriteLine(" Type: {0} \n Name: {1} \n Value: {2} \n AtributeCount: {3} \n", 
+                nodeType, nodeName, nodeValue, nodeAtributeCount
+            );
+
+            reader.WhitespaceHandling = WhitespaceHandling.None;
+
+            // GetAttribute
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element && reader.AttributeCount > 0)
+                {
+                    Console.WriteLine(" id:\t   {0}", reader.GetAttribute("id"));
+                    Console.WriteLine(" title:\t   {0}", reader.GetAttribute("title"));
+                    Console.WriteLine(" category: {0}", reader.GetAttribute("category"));
+
+                    Console.WriteLine();
+                }
+            }
+            reader.Close();
+
+            Console.WriteLine("MoveToNextAttribute");
+            XmlTextReader reader2 = new XmlTextReader(@"../../Data/data.xml");
+
+            while (reader2.Read())
+            {
+                if (reader2.NodeType == XmlNodeType.Element && reader2.AttributeCount > 0)
+                {
+                    while (reader2.MoveToNextAttribute())
+                    {
+                        Console.WriteLine(" {0, 10}: {1}",
+                            reader2.Name, reader2.Value
+                        );
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            reader2.Close();
+
         }
     }
 }
