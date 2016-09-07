fce538697a3dda0d01782fe6118b8fed9fb73819
Nojes <vv.nojenko@gmail.com>
2016-09-07 10:26:30 +0300

    BinaryFormatter

--- lesson-16/Serialization/01-Ex1/Program.cs
+++ lesson-16/Serialization/01-Ex1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,21 @@ namespace _01_Ex1
     {
         static void Main(string[] args)
         {
+            const string DATA_ROOT = "../../Data/";
+
+            Student s1 = new Student("Vasya", "Student", 21);
+            s1.ConsoleWrite();
+
+            FileStream fs = new FileStream(DATA_ROOT + "student.dat", FileMode.Create, FileAccess.Write, FileShare.None);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, s1);
+            fs.Close();
+
+            FileStream fs2 = new FileStream(DATA_ROOT + "student.dat", FileMode.Open, FileAccess.Read, FileShare.None);
+            Student s2 = (Student)bf.Deserialize(fs2);
+            fs2.Close();
+
+            s2.ConsoleWrite();
         }
     }
 }
---
+++ lesson-16/Serialization/01-Ex1/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Ex1
+{
+    [Serializable]
+    class Student
+    {
+        static uint staticId = 0;
+
+        public uint Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public uint Age { get; set; }
+
+        public Student(string fname, string lname, uint age)
+        {
+            this.Id = ++staticId;
+            this.Fname = fname;
+            this.Lname = lname;
+            this.Age = age;
+        }
+
+        public Student(uint id, string fname, string lname, uint age)
+        {
+            this.Id = id;
+            this.Fname = fname;
+            this.Lname = lname;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return String.Format(" {0} {1,-10} {2,-10} {3}", Id, Fname, Lname, Age);
+        }
+
+        public virtual void ConsoleWrite()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
